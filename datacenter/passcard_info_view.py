a29dbc0288ecc0ca2de4638d8c81d475474c0125
Nikolay Moroz <68197237+Adedal513@users.noreply.github.com>
2022-05-17 10:32:09 +0300

    Fixed variable naming
    Lists and Dicts of visits named properly

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -22,20 +22,20 @@ def is_visit_long(visit: Visit, minutes=60) -> bool:
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.get(passcode=passcode)
-    this_passcard_visits_query = Visit.objects.filter(passcard__passcode=passcode)
-    this_passcard_visits = []
+    this_passcard_visits_serialized = Visit.objects.filter(passcard__passcode=passcode)
+    this_passcard_visits_info = []
 
-    for visit in this_passcard_visits_query:
+    for visit in this_passcard_visits_serialized:
         visit_info = {
             'entered_at': localtime(visit.created_at),
             'duration': format_duration(visit.get_duration()),
             'is_strange': is_visit_long(visit)
         }
 
-        this_passcard_visits.append(visit_info)
+        this_passcard_visits_info.append(visit_info)
 
     context = {
         'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
+        'this_passcard_visits': this_passcard_visits_info
     }
     return render(request, 'passcard_info.html', context)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -16,19 +16,19 @@ def format_duration(delta: datetime.timedelta) -> str:
 
 
 def storage_information_view(request):
-    non_closed_visits = []
-    current_visits = Visit.objects.all().filter(leaved_at=None)
+    non_closed_visits_info = []
+    current_visits_serialized = Visit.objects.all().filter(leaved_at=None)
 
-    for visit in current_visits:
+    for visit in current_visits_serialized:
         visit_info = {
             'who_entered': visit.passcard.owner_name,
             'entered_at': localtime(visit.entered_at),
             'duration': format_duration(visit.get_duration())
         }
 
-        non_closed_visits.append(visit_info)
+        non_closed_visits_info.append(visit_info)
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': non_closed_visits_info,
     }
     return render(request, 'storage_information.html', context)
